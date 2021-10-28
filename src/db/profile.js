5aea87fe0b41fdd6b96991985ce238c6692692fb
ali turkaman <79016171+aliturkamanjs@users.noreply.github.com>
2021-10-28 07:38:30 -0700

    Update profile.js

--- src/db/profile.js
+++ src/db/profile.js
@@ -1,11 +1,9 @@
-export const profile = () => [
-  {
-    id: Math.floor(Math.random() * 52172830),
-    name: "ali turkaman",
-    headline: "frontend developer",
-    header:
-      "https://media-exp1.licdn.com/dms/image/C4E16AQHXT7E6qnPalw/profile-displaybackgroundimage-shrink_200_800/0/1634648143364?e=1640822400&v=beta&t=LTO5HZFE5h-UFiprFuDT1IHkWFb3_l7hpOGZPdctlrU",
-    profileImage:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQGDLTWirp9bDA/profile-displayphoto-shrink_200_200/0/1634990800908?e=1640822400&v=beta&t=u787Hy5Q5DgbUeotOshppEG-4e_dIHh2BV4bLbN8u38",
-  },
-];
+export const profile = {
+  id: Math.floor(Math.random() * 52172830),
+  name: "ali turkaman",
+  headline: "frontend developer",
+  header:
+    "https://media-exp1.licdn.com/dms/image/C4E16AQHXT7E6qnPalw/profile-displaybackgroundimage-shrink_200_800/0/1634648143364?e=1640822400&v=beta&t=LTO5HZFE5h-UFiprFuDT1IHkWFb3_l7hpOGZPdctlrU",
+  profileImage:
+    "https://media-exp1.licdn.com/dms/image/C4E03AQGDLTWirp9bDA/profile-displayphoto-shrink_200_200/0/1634990800908?e=1640822400&v=beta&t=u787Hy5Q5DgbUeotOshppEG-4e_dIHh2BV4bLbN8u38",
+};
