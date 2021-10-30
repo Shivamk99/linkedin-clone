88d7cea781c8425ef7ead04cf406b431a185f0fa
ali turkaman <79016171+aliturkamanjs@users.noreply.github.com>
2021-10-30 05:29:05 -0700

    Update user.js

--- src/components/rightSideBar/user.js
+++ src/components/rightSideBar/user.js
@@ -9,7 +9,7 @@ import { Button } from "@chakra-ui/button";
 
 const User = ({ userName, profile, headline, id }) => {
   return (
-    <Box mt="14px" key={id}>
+    <Box mt="14px" key={id} bg="white">
       <Flex>
         <Avatar name="Front-end Developer" src={profile} />
         <Box
